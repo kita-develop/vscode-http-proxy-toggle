8a2ca8afc55664a184bc5654a23171a74343eaa1
kita-dev <kita1976.com@gmail.com>
2024-05-13 00:03:58 +0900

    support proxy toggle
    - コマンドでproxyの設定切替

--- src/extension.ts
+++ src/extension.ts
@@ -5,22 +5,19 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    let proxySetting1 = vscode.workspace.getConfiguration('http').get<string>('proxy');
+    let proxySetting2 = 'http://proxy.example.com:80/'; // ここに2つ目のプロキシ設定を記述
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "http-proxy-toggle" is now active!');
+    let currentProxy = proxySetting1;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('http-proxy-toggle.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from http-proxy-toggle!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(vscode.commands.registerCommand('http-proxy-toggle.toggleProxy', () => {
+        if (currentProxy === ``) {
+            currentProxy = proxySetting2;
+        } else {
+            currentProxy = ``;
+        }
+        vscode.workspace.getConfiguration('http').update('proxy', currentProxy, vscode.ConfigurationTarget.Global);
+    }));
 }
-
 // This method is called when your extension is deactivated
 export function deactivate() {}
